f2fb89142084548ad2471b31650cc1887718cf8a
Tiago <tdcosta100@yahoo.com.br>
2016-09-23 02:03:51 -0300

    Adição de templates de modais, para melhor reúso de código

--- ProjetoPadrao.Web/Scripts/Site.js
+++ ProjetoPadrao.Web/Scripts/Site.js
@@ -1,6 +1,6 @@
 ﻿jQuery(function ($)
 {
-    $(".modal").on("hidden.bs.modal", function (event)
+    $(document).on("hidden.bs.modal", ".modal", function (event)
     {
         $(this).removeClass("modal-stack");
 
@@ -10,33 +10,150 @@
         }
     });
 
-    $(".modal").on("show.bs.modal", function (event)
+    $(document).on("show.bs.modal", ".modal", function (event)
     {
         if ($(this).hasClass("modal-stack"))
         {
             return;
         }
 
-        $(this).addClass("modal-stack").css("z-index", 1040 + 10 * $(".modal-stack").length);
+        var highestZIndex = 1040;
+
+        $(".modal-stack").each(function ()
+        {
+            var zIndex = parseInt($(this).css("zIndex"));
+            
+            if (zIndex > highestZIndex)
+            {
+                highestZIndex = zIndex;
+            }
+        });
+
+        $(this).addClass("modal-stack").css("z-index", highestZIndex + 10);
 
         setTimeout(function ()
         {
-            $(".modal-backdrop").not(".modal-backdrop-stack").css("z-index", 1035 + 10 * ($(".modal-backdrop.modal-backdrop-stack").length + 1)).addClass("modal-backdrop-stack");
+            $(".modal-backdrop").not(".modal-backdrop-stack").css("z-index", highestZIndex + 5).addClass("modal-backdrop-stack");
         }, 50);
     });
 });
 
 function Modal(options)
 {
-	var defaults = {
-		id: "",
+    var defaults = {
+        attributes: {
+            "id": null,
+            "class": "modal fade",
+            "tabindex": -1,
+            "role": "dialog",
+            "aria-labelledby": null
+        },
 		title: "",
+		content: "",
+		initialize: true,
+        removeOnDismiss: true,
 		backdrop: true,
 		keyboard: true,
 		show: true,
-		content: "",
 		buttons: []
 	};
 
+	var buttonDefaults = {
+	    content: "",
+	    attributes: {
+	        "id": null,
+            "type": "button",
+	        "class": "btn btn-primary",
+	        "aria-pressed": null,
+            "data-dismiss": null,
+	        "data-toggle": null,
+	        "data-target": null
+	    },
+	    events: {
+	        "click": function (event)
+	        {
+
+	        }
+	    }
+	};
+
 	var settings = $.extend({}, defaults, options);
+
+	var modalElement = $("#modal-template").clone().attr(settings.attributes);
+
+	$(".modal-title", modalElement).html(settings.title);
+	$(".modal-body", modalElement).html(settings.content);
+
+	$(settings.buttons).each(function ()
+	{
+        var buttonSettings = $.extend(true, {}, buttonDefaults, this)
+
+        var button = $("<button />").html(buttonSettings.content).attr(buttonSettings.attributes).on(buttonSettings.events).appendTo($(".modal-footer", modalElement)   );
+	});
+
+	if (settings.removeOnDismiss)
+	{
+	    modalElement.on("hidden.bs.modal", function ()
+	    {
+	        $(this).remove();
+	    });
+	}
+
+	if (settings.initialize)
+	{
+	    modalElement.appendTo("body").modal({
+	        backdrop: settings.backdrop,
+	        keyboard: settings.keyboard,
+            show: settings.show
+	    });
+	}
+
+	return modalElement;
+}
+
+function ModalSuccess(title, content, buttonContent)
+{
+    return Modal({
+        title: title,
+        content: content,
+        buttons: [
+            {
+                content: buttonContent,
+                attributes: {
+                    "class": "btn btn-success",
+                    "data-dismiss": "modal"
+                }
+            }
+        ]
+    });
+}
+
+function ModalConfirmCancel(title, content, confirmContent, cancelContent, clickConfirm, clickCancel)
+{
+    return Modal({
+        title: title,
+        content: content,
+        buttons: [
+            {
+                content: cancelContent,
+                attributes: {
+                    "class": "btn btn-danger",
+                    "data-dismiss": "modal"
+                },
+                events: {
+                    click: clickCancel || function() {}
+                }
+            },
+            {
+                content: confirmContent,
+                attributes: {
+                    "class": "btn btn-success",
+                    "data-dismiss": "modal"
+                },
+                events: {
+                    click: clickConfirm || function() {}
+                }
+            }
+        ]
+    });
 }
