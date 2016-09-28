a1e822be20924a71d253e822e0b99346e9620530
Tiago <tdcosta100@yahoo.com.br>
2016-09-28 02:16:36 -0300

    Adição de metadados nas classes de bancos de dados

---
+++ ProjetoPadrao.Dados/Entidades/Extensoes/Conteudo.cs
@@ -0,0 +1,17 @@
+﻿namespace ProjetoPadrao.Dados.Entidades
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    [MetadataType(typeof(ConteudoMetadata))]
+    public partial class Conteudo
+    {
+    }
+
+    public class ConteudoMetadata
+    {
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy HH:mm}")]
+        public Nullable<System.DateTime> DataPublicacao { get; set; }
+    }
+}
